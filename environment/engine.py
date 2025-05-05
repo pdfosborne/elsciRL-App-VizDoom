323be4dad1ae2d131a610d21fe073fb5dab242bd
Philip Osborne <pdfosborne@gmail.com>
2025-05-05 14:36:46 +0100

    Update engine.py get obs from env state function

--- environment/engine.py
+++ environment/engine.py
@@ -11,23 +11,50 @@ class Engine:
         self.game.init()
         self.state_history = []
         self.action_history = []
+    
+    def __collect_observations(self):
+        observation = {}
+        if self.state is not None:
+            observation["screen"] = self.state.screen_buffer
+            if self.channels == 1:
+                observation["screen"] = self.state.screen_buffer[..., None]
+            if self.depth:
+                observation["depth"] = self.state.depth_buffer[..., None]
+            if self.labels:
+                observation["labels"] = self.state.labels_buffer[..., None]
+            if self.automap:
+                observation["automap"] = self.state.automap_buffer
+                if self.channels == 1:
+                    observation["automap"] = self.state.automap_buffer[..., None]
+            if self.num_game_variables > 0:
+                observation["gamevariables"] = self.state.game_variables.astype(
+                    np.float32
+                )
+        else:
+            # there is no state in the terminal step, so a zero observation is returned instead
+            for space_key, space_item in self.observation_space.spaces.items():
+                observation[space_key] = np.zeros(
+                    space_item.shape, dtype=space_item.dtype
+                )
 
+        return observation
+    
     def reset(self, start_obs=None):
         self.game.new_episode()
         self.state_history = []
         self.action_history = []
-        state = self.game.get_state()
-        print("START STATE:", state)
-        return state
+        self.state = self.game.get_state()
+        
+        return self.__collect_observations()
 
     def step(self, state, action):
         reward = self.game.make_action(action)
         terminated = self.game.is_episode_finished()
         info = {}
-        next_state = None if terminated else self.game.get_state()
-        self.state_history.append(state)
+        self.state = None if terminated else self.game.get_state()
+        self.state_history.append(self.state)
         self.action_history.append(action)
-        return next_state, reward, terminated, info
+        return self.__collect_observations(), reward, terminated, info
 
     def legal_move_generator(self, state=None):
         return list(range(self.game.get_available_buttons_size()))
